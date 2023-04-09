2d8d00be2d2c7c2df6f2a9cb5a6be1665292bb17
whiiiite <113050605+whiiiite@users.noreply.github.com>
2023-04-09 16:11:20 +0300

    Update MotherboardInfo.cs

--- WinConfigInfo/MotherboardInfo.cs
+++ WinConfigInfo/MotherboardInfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Management;
-using System.Text;
-using System.Threading.Tasks;
 using WinConfigInfo.Interfaces;
 
 namespace WinConfigInfo
